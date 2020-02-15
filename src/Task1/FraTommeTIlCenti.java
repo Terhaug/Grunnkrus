3a33c17300f1b14aff6815caeedc0b4b58d21d5b
terhaug <terjehau95@gmail.com>
2020-02-15 01:16:13 +0100

    Small change to first task

--- src/Task1/FraTommeTIlCenti.java
+++ src/Task1/FraTommeTIlCenti.java
@@ -2,12 +2,13 @@ package Task1;
 
 import static java.lang.Double.parseDouble;
 import static javax.swing.JOptionPane.showInputDialog;
+import static javax.swing.JOptionPane.showMessageDialog;
 
 public class FraTommeTIlCenti {
     public static void main(String[] args) {
         double tommePrefix = 2.54;
         double antTomme = parseDouble(showInputDialog("Antall tomme"));
-        System.out.println(antTomme + " = " + antTomme*tommePrefix + " Centimeter");
+        showMessageDialog(null,"antTomme" + " = " + antTomme*tommePrefix + " Centimeter");
     }
 
 }
